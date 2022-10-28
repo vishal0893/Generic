88a67b8dca6bad3732435f061e0542d61faa1ad3
vishal0893 <113405560+vishal0893@users.noreply.github.com>
2022-10-28 16:35:00 +0530

    Generics in class
    this project shows how Generic can be applied at class level

---
+++ GenericInClass/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace GenericInClass;
+class GenericInClass
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("the max value of integer is");
+        Console.WriteLine(ClassInGeneric<int>.GetMax(23, 56, 99));
+        Console.WriteLine("The max value of float is");
+        Console.WriteLine(ClassInGeneric<Double>.GetMax(.99, 5.5, 2.5));
+        Console.WriteLine("The max value of string is");
+        Console.WriteLine(ClassInGeneric<String>.GetMax("x", "L", "D"));
+    }
+}
