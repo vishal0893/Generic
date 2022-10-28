93825c5b3306bd8e2c05796a81f61bf2bede4650
vishal yadav <vishal.yv0893@gmail.com>
2022-10-28 16:19:28 +0530

    finding maximum value

---
+++ FindMaximum.cs/GenericMaximum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximum.cs
+{
+    class GenericMaximum<T> where T:IComparable
+    {
+        public T[] value;
+        public GenericMaximum(T[] value)
+        {
+            this.value = value;
+        }
+        public void toPrint<T>(T[] value)
+        {
+            foreach(var item in value)
+            {
+                Console.Write(item);
+            }
+            Console.Write("---------------");
+        }
+    }
+}
---
+++ FindMaximum.cs/Program.cs
@@ -0,0 +1,16 @@
+﻿using FindMaximum.cs;
+using System;
+using System.Collections.Generic;
+namespace FindMax;
+class GenericMax
+{
+    public static void Main(string arr)
+    {
+        int[] intArray = { 10, 20, 30, 40, 50 };
+        char[] charArray = { 'H', 'e', 'l', 'l', 'o' };
+        double[] doubleArray = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+        Console.WriteLine("Find max Number");
+        int output = FindMaximumNumber.MaximumIntegerNumber(11, 22, 33);
+        Console.WriteLine(output);
+    }
+}
