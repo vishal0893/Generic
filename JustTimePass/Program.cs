58826d6fa755e39b42423523b3f1da1b2738885a
vishal0893 <113405560+vishal0893@users.noreply.github.com>
2022-10-28 16:26:29 +0530

    Add files via upload

---
+++ JustTimePass/Program.cs
@@ -0,0 +1,23 @@
+﻿using JustTimePass;
+using System;
+namespace GenericsDemo;
+class MainClass
+{
+    public static void Main()
+    {
+        //array initialization
+        int[] intArray = { 32, 45, 67 };
+        double[] doubleArray = { 1.1, 2.2, 3.3 };
+        char[] charArray = { 'h', 'e', 'l', 'l', 'o' };
+        UCprintmax Obj = new UCprintmax();
+        //Calling Array via simple methods
+       // Obj.toPrint(intArray);
+       // Obj.toPrint(doubleArray);
+        //Obj.toPrint(charArray);
+        //calling array via generics method
+        Obj.toPrint<int>(intArray);
+        Obj.toPrint<char>(charArray);
+        Obj.toPrint<double>(doubleArray);
+
+    }
+}
---
+++ JustTimePass/UCprintmax.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustTimePass
+{
+   class UCprintmax
+    {
+        public void toPrint<T>(T[] inputArray)
+        {
+            foreach(T element in inputArray)
+            {
+                Console.Write(element);
+
+            }
+            Console.Write("-----------------");
+        }
+        public void toPrint(int[] intArray)
+        {
+            foreach(int element in intArray)
+            {
+                Console.Write(element);
+            }
+            Console.Write("---------------");
+        }
+        public void toPrint(double[] doubleArray)
+        {
+            foreach (double element in doubleArray)
+            {
+                Console.Write(element);
+            }
+            Console.Write("---------------");
+        }
+        public void toPrint(char[] charArray)
+        {
+            foreach(char element in charArray)
+            {
+                Console.Write(element);
+            }
+            Console.Write("---------------");
+        }
+    }
+}
