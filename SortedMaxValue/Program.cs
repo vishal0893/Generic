ad8ba7f4209260db4ec810b568770b68cc9876c7
vishal0893 <113405560+vishal0893@users.noreply.github.com>
2022-10-29 08:17:46 +0530

    Add files via upload

---
+++ SortedMaxValue/Program.cs
@@ -0,0 +1,15 @@
+﻿using SortedMaxValue;
+using System;
+namespace GenericSorted;
+class Program
+{
+    public static void Main()
+    {
+        int[] arr = { 233, 456, 6786, 453, 5667 };
+        double[] arg = { 1.22, 43.5, 67.5, 34.67, 33.33 };
+        MaxGenericSort<int> Gen = new MaxGenericSort<int>(arr);
+        MaxGenericSort<Double> max = new MaxGenericSort<Double>(arg);
+        max.PrintValue();
+        Gen.PrintValue();
+    }
+}
